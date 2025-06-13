20c81622636942c1c99f0ae53509bc3559b9db55
PatrickMatheus <patrick.lima@ufam.edu.br>
2025-06-13 17:36:46 -0400

    Adicionando atividade 3 (Imagem 50% Azul e 50% vermelha)

---
+++ atividade_3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ofstream imagem_entrada; 
+
+    imagem_entrada.open("./images/imagem.ppm");
+    if(imagem_entrada.is_open()){
+
+        //Head
+        imagem_entrada << "P3" << endl;
+        imagem_entrada << "70 50" << endl;
+        imagem_entrada << "255" << endl;
+        
+        //corpo
+        for(int i = 0; i < 50; i++){
+            for(int j = 0; j < 70; j++){
+                if(j < 35){
+                    imagem_entrada << "255 0 0 ";
+                }else{
+                    imagem_entrada << "0 0 255 ";
+                }
+            }
+            imagem_entrada << endl;
+        }
+
+    }
+
+    imagem_entrada.close();
+
+    return 0;
+}
